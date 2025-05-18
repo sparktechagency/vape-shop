717d95177065f0a1bf60307901f89c109b369e69
Kazi Omar Faruk <kaziomar144@gmail.com>
2025-05-18 11:05:06 +0600

    Forum and post comment

--- app/Services/CommentsService.php
+++ app/Services/CommentsService.php
@@ -13,5 +13,21 @@ class CommentsService
         $this->repository = $repository;
     }
 
-    // Define service methods that use the repository
+    //get all comments in a post
+    public function getAllComments($postId, $modelType)
+    {
+        return $this->repository->getCommentsByPostId($postId, $modelType);
+    }
+
+    //add comment
+    public function createComment(array $data)
+    {
+        return $this->repository->createComment($data);
+    }
+
+    //delete comment
+    public function deleteComment($commentId)
+    {
+        return $this->repository->deleteComment($commentId);
+    }
 }
