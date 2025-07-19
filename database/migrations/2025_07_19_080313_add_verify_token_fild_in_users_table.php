ac80b1a50b402d461d381fadba34e983249d2c69
Kazi Omar Faruk <kaziomar144@gmail.com>
2025-07-19 15:55:05 +0600

    change email link

---
+++ database/migrations/2025_07_19_080313_add_verify_token_fild_in_users_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('verify_token', 40)->nullable()->after('otp')->comment('Token for email verification');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('verify_token');
+        });
+    }
+};
--- resources/views/emails/OTP.blade.php
+++ resources/views/emails/OTP.blade.php
@@ -5,15 +5,22 @@ Hello {{ $data['name'] }},
 
 Your One-Time Password (OTP) is:
 
-## **{{$data['otp']}}**
+## **{{ $data['otp'] }}**
 
-This OTP is valid for **{{$data['otp_expire_time']}}** minutes.
+This OTP is valid for **{{ $data['otp_expire_time'] }} minutes**.
+
+@component('mail::button', ['url' => url('/verify-otp?email=' . urlencode($data['email']) . '&otp=' . urlencode($data['otp']))])
+Verify OTP Now
+@endcomponent
 
 ---
 
+If the OTP has expired, you can request a new one by clicking the link below:
+
+[Resend OTP]({{ url('/verify-otp?email=' . urlencode($data['email'])) }})
+
 If you did not request this, please ignore this message.
 
-Thank you, <br>
+Thanks,<br>
 **{{ config('app.name') }}** Team
-
 @endcomponent
